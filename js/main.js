2f37d37e45a956c02cfe39d96eabfe1e24ca05e6
Constantin Panaitescu <chrisfeatherp@gmail.com>
2014-10-30 21:51:45 +0100

    Interactive example

--- js/main.js
+++ js/main.js
@@ -1,10 +1,50 @@
-var app = angular.module('app', ['uiComponents']);
+var app = angular.module('app',['uiComponents']);
 
+app.controller('SelectController', ['$scope', function($scope){
 
-app.controller('SelectController', ['$scope', function($scope) {
-    
-	$scope.options = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
+    $scope.options = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
     $scope.selectedOption = 'Select an option';
 
-}]);
+    $scope.addValue =  function () {
+        $('.dupe').hide();
+        $('.empty').hide();
+        if(typeof ($scope.newValue) !="undefined" && $scope.newValue ) {
 
+            if($scope.options.indexOf($scope.newValue) == -1) {
+                console.log($scope.options.indexOf($scope.newValue));
+                $scope.options.push($scope.newValue);
+                $scope.newValue = null;
+            }else{
+                $('.dupe').show();
+            }
+        }else{
+            $('.empty').show();
+        }
+    }
+
+
+
+    // indexOf implementation for IE
+    var indexOf = function(needle) {
+        if(typeof Array.prototype.indexOf === 'function') {
+            indexOf = Array.prototype.indexOf;
+        } else {
+            indexOf = function(needle) {
+                var i = -1, index = -1;
+
+                for(i = 0; i < this.length; i++) {
+                    if(this[i] === needle) {
+                        index = i;
+                        break;
+                    }
+                }
+
+                return index;
+            };
+        }
+
+        return indexOf.call(this, needle);
+    };
+
+    
+}])
